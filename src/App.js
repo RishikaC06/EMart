6bff68e69750bc5808c38fe5a836aac123770bdc
RishikaC06 <rishikac2002@gmail.com>
2024-02-23 21:25:51 +0530

    Fix path of cart sign in and register

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
         <Route path='/products/:id' exact element={<Product/>}/>
         <Route path="/products" element={<Products/>} />
         <Route path="/categories/:name" element={<CategoryProducts/>} />
-        <Route path="Login" element={<Login/>}/>
-        <Route path="Register" element={<Register/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Register" element={<Register/>}/>
         <Route path="cart" element={<Cart/>} />
       </Routes>
       <Footer/>
